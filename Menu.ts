4ce0d04c1fb27b83b69693bf622660eb041e549b
Joao <oliveira110965@gmail.com>
2026-02-03 14:18:13 -0300

    Adicionando README

--- Menu.ts
+++ Menu.ts
@@ -33,46 +33,50 @@ function main() {
         opcao = leia.questionInt("");
 
         if (opcao == 9) {
-            console.log("\nBanco do Brazil com Z - O seu Futuro começa aqui!");
+            console.log(colors.fg.greenstrong,
+                "\nBanco do Brazil com Z - O seu Futuro começa aqui!");
             sobre();
+            console.log(colors.reset, "");
             process.exit(0);
         }
 
         switch (opcao) {
             case 1:
-                console.log("\nCriar Conta\n");
+                console.log(colors.fg.whitestrong,
+                    "\nCriar Conta\n",colors.reset, colors.reset);
 
                 break;
             case 2:
-                console.log("\nListar todas as Contas\n");
+                console.log(colors.fg.whitestrong,
+                    "\nListar todas as Contas\n",colors.reset,colors.reset);
 
                 break;
             case 3:
-                console.log("\nConsultar dados da Conta - por número\n");
+                console.log(colors.fg.whitestrong,"\nConsultar dados da Conta - por número\n",colors.reset);
 
                 break;
             case 4:
-                console.log("\nAtualizar dados da Conta\n");
+                console.log(colors.fg.whitestrong,"\nAtualizar dados da Conta\n",colors.reset);
 
                 break;
             case 5:
-                console.log("\nApagar uma Conta\n");
+                console.log(colors.fg.whitestrong,"\nApagar uma Conta\n",colors.reset);
 
                 break;
             case 6:
-                console.log("\nSaque\n");
+                console.log(colors.fg.whitestrong,"\nSaque\n",colors.reset);
 
                 break;
             case 7:
-                console.log("\nDepósito\n");
+                console.log(colors.fg.whitestrong,"\nDepósito\n",colors.reset);
 
                 break;
             case 8:
-                console.log("\nTransferência entre Contas\n");
+                console.log(colors.fg.whitestrong,"\nTransferência entre Contas\n",colors.reset);
 
                 break;
             default:
-                console.log("\nOpção Inválida!\n");
+                console.log(colors.fg.whitestrong,"\nOpção Inválida!\n",colors.reset);
 
                 break;
         }
@@ -82,7 +86,7 @@ function main() {
 
 
 export function sobre(): void {
-    console.log("\n*****************************************************");
+    console.log("\n*,colors.reset****************************************************");
     console.log("Projeto Desenvolvido por: João Pedro ");
     console.log("Generation Brasil - generation@generation.org");
     console.log("github.com/Pedro-gomes2/conta_bancaria");
